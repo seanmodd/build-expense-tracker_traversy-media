44b1f4a9539b71f4761db447fd18c55148229140
seanmodd <sean@vestaridefair.com>
2022-02-21 14:43:52 -0800

    1 file modified

--- components/MyCounter.js
+++ components/MyCounter.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { CounterContext } from '../contexts/CounterContext';
+import { CounterContext } from '../context/CounterContext';
 
 const MyCounter = () => {
   const myCount = useContext(CounterContext);
