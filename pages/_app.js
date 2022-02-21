cb3eafce79451c4ab7a0968613002d431b458eb2
seanmodd <sean@vestaridefair.com>
2022-02-21 14:43:07 -0800

    2 files modified, 3 added, 1 deleted

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import CounterContextProvider from '../context/CounterContext';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <CounterContextProvider>
+      <Component {...pageProps} />
+    </CounterContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import Head from 'next/head';
 import Layout from '../components/Layout';
 import MyCounter from '../components/MyCounter';
-import CounterContextProvider from '../contexts/CounterContext';
+import CounterContextProvider from '../context/CounterContext';
 
 export default function Home() {
   return (
     <Layout>
-      <CounterContextProvider>
-        <MyCounter />
-      </CounterContextProvider>
+      <MyCounter />
     </Layout>
   );
 }
